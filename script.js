e113429a2c6981db766890ab73daaf2920aa0017
zi wang <ziwang55@gmail.com>
2020-11-14 20:37:36 -0600

    adding js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+document.querySelector(".quizGithubCode").onclick = codeQuizCode;
+document.querySelector(".quizLiveLink").onclick = codeQuizLive;
+document.querySelector(".")
+// Link to javascript code quiz code
+function codeQuizCode() {
+    let url = "https://github.com/ZiWang55/HW4_CodeQuiz";
+    window.open(url);
+}
+// Link to javascript code quiz live deployed link
+function codeQuizLive() {
+    let url = "https://ziwang55.github.io/HW4_CodeQuiz"
+    window.open(url);
+}

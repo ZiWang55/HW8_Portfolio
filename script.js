a996a2aac19bf8608847e4bd10cdb9ceb83c8f95
zi wang <ziwang55@gmail.com>
2020-11-14 21:16:41 -0600

    css changes and bootstrap

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-
-
 //  = codeQuizCode;
 
 // Link to javascript code quiz code
 document.querySelector(".quizGithubCode").onclick = function codeQuizCode() {
-    let url = "https://github.com/ZiWang55/HW4_CodeQuiz";
-    window.open(url);
-}
+  let url = "https://github.com/ZiWang55/HW4_CodeQuiz";
+  window.open(url);
+};
 // Link to javascript code quiz live deployed site
 document.querySelector(".quizLiveLink").onclick = function codeQuizLive() {
-    let url = "https://ziwang55.github.io/HW4_CodeQuiz"
-    window.open(url);
-}
+  let url = "https://ziwang55.github.io/HW4_CodeQuiz";
+  window.open(url);
+};
 
 // Link to weather dashboard code
 document.querySelector(".weatherGithub").onclick = function weatherDashCode() {
-    let url = "https://github.com/ZiWang55/HW6_Weather_Dashboard";
-    window.open(url);
-}
+  let url = "https://github.com/ZiWang55/HW6_Weather_Dashboard";
+  window.open(url);
+};
 // Link to weather dashboard live deployed site
 document.querySelector(".weatherLive").onclick = function weatherDashLive() {
-    let url = "https://ziwang55.github.io/HW6_Weather_Dashboard";
-    window.open(url);
-}
+  let url = "https://ziwang55.github.io/HW6_Weather_Dashboard";
+  window.open(url);
+};

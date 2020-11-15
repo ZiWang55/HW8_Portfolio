2da4146dfffa834716f392a095be6a33f981b6ab
zi wang <ziwang55@gmail.com>
2020-11-14 21:45:02 -0600

    simplify js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 // Link to Beer Day Helper github code
-document.querySelector(".beerDayCode").onclick = function beerDayCode() {
-    let url = "https://github.com/ryan-conran/Beer-Day-Helper";
-    window.open(url);
-}
+document.querySelector(".beerDayCode").onclick = function() {
+  let url = "https://github.com/ryan-conran/Beer-Day-Helper";
+  window.open(url);
+};
 // Link to Beer Day Helper live site
-document.querySelector(".beerDayliveLink").onclick = function beerDayLive() {
-    let url = "https://ryan-conran.github.io/Beer-Day-Helper";
-    window.open(url);
-}
+document.querySelector(".beerDayLiveLink").onclick = function() {
+  let url = "https://ryan-conran.github.io/Beer-Day-Helper";
+  window.open(url);
+};
+
 // Link to javascript code quiz code
-document.querySelector(".quizGithubCode").onclick = function codeQuizCode() {
+document.querySelector(".quizGithubCode").onclick = function() {
   let url = "https://github.com/ZiWang55/HW4_CodeQuiz";
   window.open(url);
 };
 // Link to javascript code quiz live deployed site
-document.querySelector(".quizLiveLink").onclick = function codeQuizLive() {
+document.querySelector(".quizLiveLink").onclick = function() {
   let url = "https://ziwang55.github.io/HW4_CodeQuiz";
   window.open(url);
 };
 
 // Link to weather dashboard code
-document.querySelector(".weatherGithub").onclick = function weatherDashCode() {
+document.querySelector(".weatherGithub").onclick = function() {
   let url = "https://github.com/ZiWang55/HW6_Weather_Dashboard";
   window.open(url);
 };
 // Link to weather dashboard live deployed site
-document.querySelector(".weatherLive").onclick = function weatherDashLive() {
+document.querySelector(".weatherLive").onclick = function() {
   let url = "https://ziwang55.github.io/HW6_Weather_Dashboard";
   window.open(url);
 };

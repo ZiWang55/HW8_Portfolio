553a0cba42a6a54f292fcb48fb6a36f7fc1dd285
zi wang <ziwang55@gmail.com>
2020-11-14 20:45:10 -0600

    adding link to weather dashboard code in js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
 
-document.querySelector(".quizGithubCode").onclick = codeQuizCode;
-document.querySelector(".quizLiveLink").onclick = codeQuizLive;
-document.querySelector(".")
+//  = codeQuizCode;
+
 // Link to javascript code quiz code
-function codeQuizCode() {
+document.querySelector(".quizGithubCode").onclick = function codeQuizCode() {
     let url = "https://github.com/ZiWang55/HW4_CodeQuiz";
     window.open(url);
 }
-// Link to javascript code quiz live deployed link
-function codeQuizLive() {
+// Link to javascript code quiz live deployed site
+document.querySelector(".quizLiveLink").onclick = function codeQuizLive() {
     let url = "https://ziwang55.github.io/HW4_CodeQuiz"
     window.open(url);
 }
+
+// Link to weather dashboard code
+document.querySelector(".weatherGithub").onclick = function weatherDashCode() {
+    let url = "https://github.com/ZiWang55/HW6_Weather_Dashboard";
+    window.open(url);
+}
+// Link to weather dashboard live deployed site
+document.querySelector(".weatherLive").onclick = weatherDashLive;
